40853177ce1d942c5825d8629d54b53d2785ccbd
Luca Cadamuro <lc.cadamuro@gmail.com>
2016-06-19 18:03:29 +0200

    structure for automated das query and intersection with JSON

---
+++ TauTriggerValidation/utils/configure.py
@@ -0,0 +1,63 @@
+## script to configure the Tau Validation tool using a run range
+## L. Cadamuro (LLR) , June 2016
+
+import argparse
+import os, sys
+from subprocess import Popen, PIPE
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Command line parser')
+    parser.add_argument('--json',          dest='jsonFile',     help='json file name',                              default=None)
+    parser.add_argument('--tag',           dest='tag',          help='tag defining this process',                   default="tau")
+    parser.add_argument('--dataset',       dest='dataset',      help='dataset to run validation on',                default="/SingleMuon/Run2016B-PromptReco-v2/MINIAOD")
+    parser.add_argument('--userfilelist',  dest='userfilelist', help='user list of input files (no DAS query)',     default="/SingleMuon/Run2016B-PromptReco-v2/MINIAOD")
+    parser.add_argument('--run-range',     dest='runrange',     help='first and last runs to be analyzed', nargs=2, default=None)
+    args = parser.parse_args()
+
+    ################### check of the input paramters
+    if not args.runrange:
+        print "** Error: please provide run range"
+        sys.exit()
+
+    ################### list all files to be processed
+    filelist = []
+    if not args.userfilelist:
+        print "... using user file list" , args.userfilelist
+        try: ff = open(args.userfilelist)
+        except :
+            print "** Error: cannot open user file list" , args.userfilelist
+            sys.exit()
+        filelist = [line.strip() for line in ff]
+    
+    else:
+        
+        ### initialize user certificate
+        print "... initializing proxy"
+        proc = Popen ('voms-proxy-info', stdout=PIPE)
+        tmp = [word for word in proc.stdout.read ().split ('\n') if 'timeleft' in word]
+        if len (tmp) == 0 or int (tmp[0].split (':')[1]) < 10 : # hours
+            os.system ('voms-proxy-init -voms cms')
+        else: print "    > already initialized"
+
+        ### fetch file list from DAS in this run range
+        print "... retrieving file list from DAS, run range is: " , int (args.runrange[0]) , int (args.runrange[1]) , " . This could take some time"
+        # NB : this version of das_client is not working in some systems, so I'm using the version from CMSSW
+        # command = 'das_client --query="file dataset=%s run between [%i,%i]" --limit=0' % (args.dataset , int (args.runrange[0]) , int (args.runrange[1]) )
+        command = 'python das_client.py --query="file dataset=%s run between [%i,%i]" --limit=0' % (args.dataset , int (args.runrange[0]) , int (args.runrange[1]) )
+        print "    > command is: " , command
+        pipe = Popen(command, shell=True, stdout=PIPE)
+        for line in pipe.stdout:
+            filelist.append (line.strip())
+        print "... found " , len(filelist) , " files in the run range"
+    
+    ################### mask the JSON (if any provided) by the run range
+    skimmedJSON = {}
+    if args.jsonFile:
+        try: json = eval( open(args.jsonFile).read() )
+        except:
+            print "** Error: json" , args.jsonFile , "not a valid JSON"
+            sys.exit()
+        skimmedJSON = dict ([ (key, value)  for key, value in json.iteritems() if key >= args.runrange[0] and key <= args.runrange[1] ])
+    # print skimmedJSON
+
+    
